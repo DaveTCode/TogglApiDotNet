a7c2756de29727d0b825bbba1ec3be20a6709b7d
David Tyler <david.tyler@metaswitch.com>
2019-03-01 14:43:34 +0000

    Added manual test application

---
+++ TogglApi.Client.ManualTest/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using NLog;
+using TogglApi.Client.Reports;
+using TogglApi.Client.Reports.Models;
+using TogglApi.Client.Reports.Models.Response;
+
+namespace TogglApi.Client.ManualTest
+{
+    internal static class Program
+    {
+        private static readonly ILogger Log = LogManager.GetCurrentClassLogger();
+
+        private static async Task Main(string[] args)
+        {
+            var workspaceId = int.Parse(args[0]);
+            var apiToken = args[1];
+            var client = new TogglReportClient(httpClient: new HttpClient(), logger: Log);
+
+            var report =
+                await client
+                    .GetSummaryReport(
+                        config: new SummaryReportConfig<
+                            SummaryReportResponseUserGroup<SummaryReportResponseProjectSubGroup>,
+                            SummaryReportResponseProjectSubGroup>
+                        (
+                            userAgent: "test-app",
+                            workspaceId: workspaceId
+                        ),
+                        apiToken: apiToken);
+
+            Console.WriteLine(report.TotalGrand);
+
+            var weeklyReport = await client.GetWeeklyReport(new WeeklyReportConfig<WeeklyReportResponseUserGroup>
+            (
+                userAgent: "test-app",
+                workspaceId: workspaceId
+            ), apiToken: apiToken);
+
+            Console.WriteLine(weeklyReport.WeekTotals);
+            Console.Read();
+        }
+    }
+}
