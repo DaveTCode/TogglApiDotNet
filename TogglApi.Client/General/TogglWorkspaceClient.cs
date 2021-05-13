256081b36a3296acd5d24083c2a2db2e06ad9015
Richard Brunt <richard.brunt@tessella.com>
2021-05-13 17:25:59 +0100

    Update API Endpoints to use new api.track.toggl.com domain
    The existing www.toggl.com/api is getting turned off in June 2021.

--- TogglApi.Client/General/TogglWorkspaceClient.cs
+++ TogglApi.Client/General/TogglWorkspaceClient.cs
@@ -15,13 +15,13 @@ namespace TogglApi.Client.General
 
         public async Task<List<Workspace>> GetWorkspaces(string apiToken)
         {
-            return await GetGenericTogglApi<List<Workspace>>(url: "https://www.toggl.com/api/v8/workspaces",
+            return await GetGenericTogglApi<List<Workspace>>(url: "https://api.track.toggl.com/api/v8/workspaces",
                 apiToken: apiToken).ConfigureAwait(false);
         }
 
         public async Task<Workspace> GetWorkspace(string apiToken, long workspaceId)
         {
-            return (await GetGenericTogglApi<WorkspaceContainer>(url: $"https://www.toggl.com/api/v8/workspaces/{workspaceId}",
+            return (await GetGenericTogglApi<WorkspaceContainer>(url: $"https://api.track.toggl.com/api/v8/workspaces/{workspaceId}",
                 apiToken: apiToken).ConfigureAwait(false)).Workspace;
         }
 
@@ -38,19 +38,19 @@ namespace TogglApi.Client.General
         public async Task<List<User>> GetUsers(string apiToken, long workspaceId)
         {
             return await GetGenericTogglApi<List<User>>(
-                url: $"https://www.toggl.com/api/v8/workspaces/{workspaceId}/users", apiToken: apiToken).ConfigureAwait(false);
+                url: $"https://api.track.toggl.com/api/v8/workspaces/{workspaceId}/users", apiToken: apiToken).ConfigureAwait(false);
         }
 
         public async Task<List<Models.Response.Client>> GetClients(string apiToken, long workspaceId)
         {
             return await GetGenericTogglApi<List<Models.Response.Client>>(
-                url: $"https://www.toggl.com/api/v8/workspaces/{workspaceId}/users", apiToken: apiToken).ConfigureAwait(false);
+                url: $"https://api.track.toggl.com/api/v8/workspaces/{workspaceId}/users", apiToken: apiToken).ConfigureAwait(false);
         }
 
         public async Task<List<Tag>> GetTags(string apiToken, long workspaceId)
         {
             return await GetGenericTogglApi<List<Tag>>(
-                url: $"https://www.toggl.com/api/v8/workspaces/{workspaceId}/tags", apiToken: apiToken).ConfigureAwait(false);
+                url: $"https://api.track.toggl.com/api/v8/workspaces/{workspaceId}/tags", apiToken: apiToken).ConfigureAwait(false);
         }
 
         public async Task<List<Models.Response.Task>> GetTasks(string apiToken, WorkspaceTaskRequestConfig config)
